63e26bc920411e1ff803c3e08809e43fad7ba707
LNSSPsd <lnsspsd@madisoncollege.edu>
2018-08-06 19:08:03 +0800

    New features

--- plugins/sample/main.c
+++ plugins/sample/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void onload(){
+void init(){
 printf("Plugin Sample: Hello World!\n\n");
 }
 
-//More functions coming soon...
+void oninitdone(){
+  printf("Init done\n\n");
+}
+
+void onclientconnected(){
+  printf("client connected\n\n");
+}
+
+void onchat(char *msg){
+  printf("Player chat: %s\n\n",msg);
+}
