242f1775c8948d112b5d76e2c762255625208207
LNSSPsd <lnsspsd@madisoncollege.edu>
2018-10-11 18:58:31 +0800

    Fixed a bug: myagentctl dn work

--- ctlmyagent.js
+++ ctlmyagent.js
@@ -15,18 +15,18 @@ try{
 	}
 }catch(nn){}
 
-if(process.argv.length==1){
+if(process.argv.length==2){
 	console.log("MyAgent Control\nmyagentctl set <settings> <set>\nmyagentctl rmconf\n%s",JSON.stringify(settings));
 }
 
-if(process.argv[1]=="set" && process.argv.length==4){
+if(process.argv[1]=="set" && process.argv.length==5){
 	try{
-	eval("settings."+process.argv[2]+"="+process.argv[3]+";");
+	eval("settings."+process.argv[3]+"="+process.argv[4]+";");
 	}catch(h){console.log("Failed to set.");}
 	console.log("Done");
 }
 
-if(process.argv[1]=="rmconf"){
+if(process.argv[2]=="rmconf"){
 	fs.unlinkSync(process.env.home+"/.myagentcfg");
 	process.exit(0);
 }
