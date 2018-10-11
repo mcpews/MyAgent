154d38054095a270179577495d4ed20d63853c43
LNSSPsd <lnsspsd@madisoncollege.edu>
2018-10-11 19:02:23 +0800

    Fix

--- ctlmyagent.js
+++ ctlmyagent.js
@@ -17,13 +17,15 @@ try{
 
 if(process.argv.length==2){
 	console.log("MyAgent Control\nmyagentctl set <settings> <set>\nmyagentctl rmconf\n%s",JSON.stringify(settings));
+	process.exit(0);
 }
 
-if(process.argv[1]=="set" && process.argv.length==5){
+if(process.argv[2]=="set" && process.argv.length==5){
 	try{
 	eval("settings."+process.argv[3]+"="+process.argv[4]+";");
 	}catch(h){console.log("Failed to set.");}
 	console.log("Done");
+	process.exit(0);
 }
 
 if(process.argv[2]=="rmconf"){
